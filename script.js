3f5c5dd0e7f40321802b1336ecc8ae1899000276
Yuriy Shikov <shikov.yuri@gmail.com>
2021-01-24 15:02:21 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,77 @@
+"use strict";
+
+const submit = document.querySelector(".button");
+
+const revenue1 = document.getElementById("revenue1");
+const x11 = document.getElementById("x11");
+const x12 = document.getElementById("x12");
+const x13 = document.getElementById("x13");
+const x14 = document.getElementById("x14");
+const x15 = document.getElementById("x15");
+
+const revenue2 = document.getElementById("revenue2");
+const x21 = document.getElementById("x21");
+const x22 = document.getElementById("x22");
+const x23 = document.getElementById("x23");
+const x24 = document.getElementById("x24");
+const x25 = document.getElementById("x25");
+
+const revenue3 = document.getElementById("revenue3");
+const x31 = document.getElementById("x31");
+const x32 = document.getElementById("x32");
+const x33 = document.getElementById("x33");
+const x34 = document.getElementById("x34");
+const x35 = document.getElementById("x35");
+
+const revenue4 = document.getElementById("revenue4");
+const x41 = document.getElementById("x41");
+const x42 = document.getElementById("x42");
+const x43 = document.getElementById("x43");
+const x44 = document.getElementById("x44");
+const x45 = document.getElementById("x45");
+
+const revenue5 = document.getElementById("revenue5");
+const x51 = document.getElementById("x51");
+const x52 = document.getElementById("x52");
+const x53 = document.getElementById("x53");
+const x54 = document.getElementById("x54");
+const x55 = document.getElementById("x55");
+
+function submitData(e) {
+  e.preventDefault();
+
+  console.log(+revenue1.value);
+  console.log(+x11.value);
+  console.log(+x12.value);
+  console.log(+x13.value);
+  console.log(+x14.value);
+  console.log(+x15.value);
+
+  console.log(+revenue2.value);
+  console.log(+x21.value);
+  console.log(+x22.value);
+  console.log(+x23.value);
+  console.log(+x24.value);
+  console.log(+x25.value);
+
+  console.log(+revenue3.value);
+  console.log(+x31.value);
+  console.log(+x32.value);
+  console.log(+x33.value);
+  console.log(+x34.value);
+  console.log(+x35.value);
+
+  console.log(+revenue4.value);
+  console.log(+x41.value);
+  console.log(+x42.value);
+  console.log(+x43.value);
+  console.log(+x44.value);
+  console.log(+x45.value);
+
+  console.log(+revenue5.value);
+  console.log(+x51.value);
+  console.log(+x52.value);
+  console.log(+x53.value);
+  console.log(+x54.value);
+  console.log(+x55.value);
+}
